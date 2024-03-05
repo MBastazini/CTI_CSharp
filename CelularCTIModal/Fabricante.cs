95771b439888d8a94c89a22d0b46574bf7a88cdd
Mateus Bastazini Nascimento Talhacolo <112733464+MBastazini@users.noreply.github.com>
2024-03-05 10:53:35 -0300

    Adicionar arquivos de projeto.

---
+++ CelularCTIModal/Fabricante.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CelularCTI.Modal
+{
+    public class Fabricante
+    {
+        //Atributos internos
+        private Int64 id_Fabricante;
+        private string nome;
+        private string descricao;
+
+        public Int64 Id_Fabricante 
+        { 
+            get { return id_Fabricante; } 
+            set { id_Fabricante = value; }
+        }
+
+        public string Nome
+        { 
+            get { return nome; } 
+            set {  nome = value; } 
+        }
+
+        public string Descricao
+        {
+            get { return descricao; } 
+            set { descricao = value; } 
+        }
+    }
+}
